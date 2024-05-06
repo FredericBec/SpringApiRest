52a88b990d35f6163a26fff3001657f7b047c88e
Frederic Bec <bec.frederic90@outlook.fr>
2024-05-06 11:18:11 +0200

    First commit

---
+++ src/main/java/fr/fms/SpringApiRest/SpringApiRestApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.SpringApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+
+}
