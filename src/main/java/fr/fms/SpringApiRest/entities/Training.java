e135ae56ef1faac0f47f28ca4ef71af3eb83c9ba
Frederic Bec <bec.frederic90@outlook.fr>
2024-05-06 14:01:28 +0200

    Add training component

---
+++ src/main/java/fr/fms/SpringApiRest/entities/Training.java
@@ -0,0 +1,23 @@
+package fr.fms.SpringApiRest.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class Training implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+}
