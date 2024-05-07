0c402576ea1a63234295ea50ff677e491fbf7d99
Frederic Bec <bec.frederic90@outlook.fr>
2024-05-07 13:25:49 +0200

    Add category entity relationship

--- src/main/java/fr/fms/SpringApiRest/entities/Training.java
+++ src/main/java/fr/fms/SpringApiRest/entities/Training.java
@@ -1,14 +1,13 @@
 package fr.fms.SpringApiRest.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -20,4 +19,9 @@ public class Training implements Serializable {
     private String description;
     private double price;
     private int quantity;
+    private String image;
+
+    @ManyToOne
+    @JsonIgnoreProperties(value = "trainings")
+    private Category category;
 }
