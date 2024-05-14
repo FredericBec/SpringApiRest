31c98eff69af3b7ea5268596da67a6aafea7a9f0
Frederic Bec <bec.frederic90@outlook.fr>
2024-05-14 13:38:30 +0200

    comment method generate data

--- src/main/java/fr/fms/SpringApiRest/SpringApiRestApplication.java
+++ src/main/java/fr/fms/SpringApiRest/SpringApiRestApplication.java
@@ -1,6 +1,8 @@
 package fr.fms.SpringApiRest;
 
+import fr.fms.SpringApiRest.dao.CategoryRepository;
 import fr.fms.SpringApiRest.dao.TrainingRepository;
+import fr.fms.SpringApiRest.entities.Category;
 import fr.fms.SpringApiRest.entities.Training;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,20 +15,27 @@ public class SpringApiRestApplication implements CommandLineRunner {
 	@Autowired
 	TrainingRepository trainingRepository;
 
+	@Autowired
+	CategoryRepository categoryRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiRestApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		generateData();
+		//generateData();
 	}
 
 	private void generateData(){
-		trainingRepository.save(new Training(null, "Java", "Java Standard Edition 8 sur 5 jours", 3500.0, 1));
-		trainingRepository.save(new Training(null, "DotNet", "DotNet & entityframework en 5 jours", 2750.0, 1));
-		trainingRepository.save(new Training(null, "PowerBi", "Business Intelligence 5 jours", 3000.0, 1));
-		trainingRepository.save(new Training(null, "NodeJs", "Prise en main de NodeJs/Express 2 jours", 1400.0, 1));
-		trainingRepository.save(new Training(null, "Php", "Initiation au Dev/Web avec hp 4 jours", 1300.0, 1));
+		Category program = new Category(null, "Language de programmation", null);
+		Category application = new Category(null, "Application", null);
+		categoryRepository.save(program);
+		categoryRepository.save(application);
+		trainingRepository.save(new Training(null, "Java", "Java Standard Edition 8 sur 5 jours", 3500.0, 1, "java.png", program));
+		trainingRepository.save(new Training(null, "DotNet", "DotNet & entityframework en 5 jours", 2750.0, 1, "dotnet_logo_icon_170223.png", program));
+		trainingRepository.save(new Training(null, "PowerBi", "Business Intelligence 5 jours", 3000.0, 1, "Power-BI-Logo-700x394.png", application));
+		trainingRepository.save(new Training(null, "NodeJs", "Prise en main de NodeJs/Express 2 jours", 1400.0, 1, "node-js.png", program));
+		trainingRepository.save(new Training(null, "Php", "Initiation au Dev/Web avec hp 4 jours", 1300.0, 1, "PHP-logo.svg.png", program));
 	}
 }
