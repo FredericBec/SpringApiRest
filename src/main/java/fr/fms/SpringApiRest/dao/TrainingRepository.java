0a194efd1ad9ca0d08f95a5ede50aa9d1048d306
Frederic Bec <bec.frederic90@outlook.fr>
2024-05-14 13:35:26 +0200

    Add category filter request

--- src/main/java/fr/fms/SpringApiRest/dao/TrainingRepository.java
+++ src/main/java/fr/fms/SpringApiRest/dao/TrainingRepository.java
@@ -2,13 +2,11 @@ package fr.fms.SpringApiRest.dao;
 
 import fr.fms.SpringApiRest.entities.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-//@CrossOrigin("*")
-//@RepositoryRestResource
+
 public interface TrainingRepository extends JpaRepository<Training, Long> {
     //List<Training> findAll();
+    List<Training> findByCategoryId(Long categoryId);
 }
