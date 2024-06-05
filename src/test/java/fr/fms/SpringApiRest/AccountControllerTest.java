d4b077ee9ae5f16fbae801b0efc1c60afd435de5
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-05 14:49:51 +0200

    Add account rest controller tests

---
+++ src/test/java/fr/fms/SpringApiRest/AccountControllerTest.java
@@ -0,0 +1,69 @@
+package fr.fms.SpringApiRest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.fms.SpringApiRest.dao.CategoryRepository;
+import fr.fms.SpringApiRest.dao.RoleRepository;
+import fr.fms.SpringApiRest.dao.TrainingRepository;
+import fr.fms.SpringApiRest.dao.UserRepository;
+import fr.fms.SpringApiRest.entities.User;
+import fr.fms.SpringApiRest.service.AccountServiceImpl;
+import fr.fms.SpringApiRest.web.AccountRestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = AccountRestController.class)
+public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AccountServiceImpl accountService;
+
+    @MockBean
+    private TrainingRepository trainingRepository;
+
+    @MockBean
+    private CategoryRepository categoryRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private RoleRepository roleRepository;
+
+    @MockBean
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Test
+    void testGetUsers() throws Exception{
+        mvc.perform(get("/users")).andExpect(status().isOk());
+    }
+
+    @Test
+    void testSaveUser() throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User newUser = new User(null, "papa", "superpapa", new ArrayList<>());
+        String requestContent = objectMapper.writeValueAsString(newUser);
+        when(accountService.saveUser(newUser)).thenReturn(newUser);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestContent))
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+}
