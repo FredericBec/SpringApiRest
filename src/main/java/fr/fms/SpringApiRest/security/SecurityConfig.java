7f79d432a48a556582f4d3661bd26d91534707d0
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-05 14:51:03 +0200

    Add configure method and password encoder

--- src/main/java/fr/fms/SpringApiRest/security/SecurityConfig.java
+++ src/main/java/fr/fms/SpringApiRest/security/SecurityConfig.java
@@ -1,4 +1,52 @@
 package fr.fms.SpringApiRest.security;
 
-public class SecurityConfig {
+import fr.fms.SpringApiRest.entities.User;
+import fr.fms.SpringApiRest.service.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private AccountServiceImpl accountService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        /*auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                User user = accountService.findUserByUsername(username);
+                Collection<GrantedAuthority> authorities = new ArrayList<>();
+                user.getRoles().forEach(role -> {
+                    authorities.add(new SimpleGrantedAuthority(role.getRolename()));
+                });
+                return new User(null, user.getUsername(), user.getPassword(), authorities);
+            }
+        });*/
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
