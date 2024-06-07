15db5893defebb639473934f8f5df4b5e3ed8a28
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-07 11:04:33 +0200

    Modify configure method

--- src/main/java/fr/fms/SpringApiRest/security/SecurityConfig.java
+++ src/main/java/fr/fms/SpringApiRest/security/SecurityConfig.java
@@ -1,47 +1,67 @@
 package fr.fms.SpringApiRest.security;
 
-import fr.fms.SpringApiRest.entities.User;
+import fr.fms.SpringApiRest.dao.UserRepository;
+import fr.fms.SpringApiRest.entities.AppUser;
 import fr.fms.SpringApiRest.service.AccountServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
-    private AccountServiceImpl accountService;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        /*auth.userDetailsService(new UserDetailsService() {
+        auth.userDetailsService(new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                User user = accountService.findUserByUsername(username);
+                AppUser user = userRepository.findByUsername(username);
                 Collection<GrantedAuthority> authorities = new ArrayList<>();
                 user.getRoles().forEach(role -> {
                     authorities.add(new SimpleGrantedAuthority(role.getRolename()));
                 });
-                return new User(null, user.getUsername(), user.getPassword(), authorities);
+                return new User(user.getUsername(), user.getPassword(), authorities);
             }
-        });*/
+        });
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests().anyRequest().authenticated();
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new JwtAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
